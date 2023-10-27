7986947d609beb8fafca166a69e0a946a5da2306
Suyash Pandey <91048475+JavaPanda30@users.noreply.github.com>
2023-10-27 08:27:24 +0530

    Add files via upload

---
+++ src/components/StudentForm.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function StudentForm(props)
+{
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [rollNo,setRollNo] = useState("");
+
+    useEffect(()=>{
+        setName(props.nameValue);
+        setEmail(props.emailValue);
+        setRollNo(props.rollNoValue);
+    },[props.nameValue,props.emailValue,props.rollNoValue]);
+
+    const arr = [name,email,rollNo];
+   
+    const handleClick = () => {
+        props.getState(arr);
+    }
+    return (
+        <div style={{maxWidth:"40%",margin:"0px auto"}}>
+            <input defaultValue={props.nameValue} onChange={(event)=>setName(event.target.value)} class="form-control my-2" placeholder="Enter your name" />
+            <input defaultValue={props.emailValue} onChange={(event)=>setEmail(event.target.value)} class="form-control my-2" placeholder="Enter your email" />
+            <input defaultValue={props.rollNoValue} onChange={(event)=>setRollNo(event.target.value)} class="form-control my-2" placeholder="Enter your roll number" />
+            <button onClick={handleClick} class="btn btn-primary my-2 d-block mx-auto" type="submit">{props.children}</button>
+        </div>
+    )
+}
+export default StudentForm;
